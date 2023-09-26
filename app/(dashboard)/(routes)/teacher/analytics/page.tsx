fb8b1645a8055d952a3e2f1ca88c2dd325e3ff67
elnur-elnur <tahirzadee@gmail.com>
2023-09-26 04:01:09 +0400

    changed text

--- app/(dashboard)/(routes)/teacher/analytics/page.tsx
+++ app/(dashboard)/(routes)/teacher/analytics/page.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const AnalyticsPage = () => {
-  return <div className="p-6">AnalyticsPage</div>;
+  return (
+    <div className="p-6">
+      <h1>Courses Analytics</h1>
+      <h2 className="text-xs text-muted-foreground mt-6">Coming soon...</h2>
+    </div>
+  );
 };
 
 export default AnalyticsPage;
