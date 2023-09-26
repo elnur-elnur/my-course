fd1cf3a2bd1cddee7f2293d5b592b0973031868a
elnur-elnur <tahirzadee@gmail.com>
2023-09-26 04:00:49 +0400

    filtering/search data-table

--- app/(dashboard)/(routes)/teacher/courses/_components/dataTable.tsx
+++ app/(dashboard)/(routes)/teacher/courses/_components/dataTable.tsx
@@ -4,9 +4,11 @@ import * as React from "react";
 
 import {
   ColumnDef,
+  ColumnFiltersState,
   SortingState,
   flexRender,
   getCoreRowModel,
+  getFilteredRowModel,
   getPaginationRowModel,
   getSortedRowModel,
   useReactTable,
@@ -21,6 +23,9 @@ import {
   TableRow,
 } from "@/components/ui/table";
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import Link from "next/link";
+import { PlusCircle } from "lucide-react";
 
 interface DataTableProps<TData, TValue> {
   columns: ColumnDef<TData, TValue>[];
@@ -32,6 +37,9 @@ export function DataTable<TData, TValue>({
   data,
 }: DataTableProps<TData, TValue>) {
   const [sorting, setSorting] = React.useState<SortingState>([]);
+  const [columnFilters, setColumnFilters] = React.useState<ColumnFiltersState>(
+    []
+  );
 
   const table = useReactTable({
     data,
@@ -40,14 +48,34 @@ export function DataTable<TData, TValue>({
     getPaginationRowModel: getPaginationRowModel(),
     onSortingChange: setSorting,
     getSortedRowModel: getSortedRowModel(),
+    onColumnFiltersChange: setColumnFilters,
+    getFilteredRowModel: getFilteredRowModel(),
     state: {
       sorting,
+      columnFilters,
     },
-
   });
 
   return (
     <div>
+      <div className="flex items-center justify-between py-4">
+        <Input
+          placeholder="Filter courses..."
+          value={(table.getColumn("title")?.getFilterValue() as string) ?? ""}
+          onChange={(event) =>
+            table.getColumn("title")?.setFilterValue(event.target.value)
+          }
+          className="max-w-sm"
+        />
+        <Link href="/teacher/create">
+          <Button>
+            {/* TODO: in md when hidden text add to button hover show text */}
+            <PlusCircle className="w-4 h-4 md:mr-2" />
+            <span className="hidden md:flex">New course</span>
+          </Button>
+        </Link>
+      </div>
+
       <div className="rounded-md border">
         <Table>
           <TableHeader>
