e807407d74087eadec88edf795c882fe5e78c506
elnur-elnur <tahirzadee@gmail.com>
2023-10-01 05:26:31 +0400

    created courses Card & List

--- app/(dashboard)/(routes)/search/page.tsx
+++ app/(dashboard)/(routes)/search/page.tsx
@@ -1,17 +1,40 @@
+import { auth } from "@clerk/nextjs";
+
 import { db } from "@/lib/db";
-
 import { Categories } from "./_components/categories";
+import { redirect } from "next/navigation";
+import { getCourses } from "@/actions/get-courses";
+import CoursesList from "@/components/courses/coursesList";
 
-const SearchPage = async () => {
+interface SearchParams {
+  searchParams: {
+    title: string;
+    categoryId: string;
+  };
+}
+
+const SearchPage = async ({ searchParams }: SearchParams) => {
+  const { userId } = auth();
+
+  // if (!userId) {
+  //   return redirect("/");
+  // }
+
   const categories = await db.category.findMany({
     orderBy: {
       name: "asc",
     },
   });
 
+  const courses = await getCourses({
+    userId, 
+    ...searchParams
+  });
+
   return (
     <div className="p-6">
       <Categories items={categories} />
+      <CoursesList items={courses} />
     </div>
   );
 };
---
+++ components/courses/courseCard.tsx
@@ -0,0 +1,34 @@
+import Image from "next/image";
+import Link from "next/link";
+
+interface CourseCardProps {
+  id: string;
+  title: string;
+  imageUrl: string;
+  chaptersLength: number;
+  price: number;
+  progress: number | null;
+  category: string;
+}
+
+const CourseCard = ({
+  id,
+  title,
+  imageUrl,
+  chaptersLength,
+  price,
+  progress,
+  category,
+}: CourseCardProps) => {
+  return (
+    <Link href={`/courses/${id}`}>
+      <div className="group hover:shadow-sm transition overflow-hidden border rounded-lg p-3 h-full">
+        <div className="relative w-full aspect-video rounded-md overflow-hidden">
+          <Image className="object-cover" fill alt={title} src={imageUrl} />
+        </div>
+      </div>
+    </Link>
+  );
+};
+
+export default CourseCard;
