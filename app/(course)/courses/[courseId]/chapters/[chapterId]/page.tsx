4b4989aa31a6bf80b47d2e4a475db46975a337a7
elnur-elnur <tahirzadee@gmail.com>
2023-10-04 02:32:13 +0400

    created chapter id page / get chapter action

---
+++ app/(course)/courses/[courseId]/chapters/[chapterId]/page.tsx
@@ -0,0 +1,40 @@
+import { getChapter } from "@/actions/get-chapter";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+const ChapterIdPage = async ({
+  params,
+}: {
+  params: { courseId: string; chapterId: string };
+}) => {
+  const { userId } = auth();
+
+  if (!userId) {
+    return redirect("/");
+  }
+
+  const {
+    chapter,
+    course,
+    muxData,
+    attachments,
+    nextChapter,
+    userProgress,
+    purchase,
+  } = await getChapter({
+    userId,
+    chapterId: params.chapterId,
+    courseId: params.courseId,
+  });
+
+  if (!chapter || !course) {
+    return redirect("/");
+  }
+
+  const isLocked = !chapter.isFree && !purchase;
+  const completeOnEnd = !!purchase && !userProgress?.isCompleted;
+
+  return <div>chapter</div>;
+};
+
+export default ChapterIdPage;
