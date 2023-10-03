dceb002b0ce215ae55239391e4def695c9d1d10e
elnur-elnur <tahirzadee@gmail.com>
2023-10-04 02:05:06 +0400

    created course: mobile sidebar, layout,navbar

--- app/(course)/courses/[courseId]/_components/courseSidebar.tsx
+++ app/(course)/courses/[courseId]/_components/courseSidebar.tsx
@@ -1,6 +1,8 @@
+import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs";
 import { Chapter, Course, UserProgress } from "@prisma/client";
 import { redirect } from "next/navigation";
+import { CourseSidebarItem } from "./courseSidebarItem";
 
 interface CourseSidebarProps {
   course: Course & {
@@ -11,16 +13,47 @@ interface CourseSidebarProps {
   progressCount: number;
 }
 
-const CourseSidebar = ({ course, progressCount }: CourseSidebarProps) => {
+const CourseSidebar = async ({ course, progressCount }: CourseSidebarProps) => {
   const { userId } = auth();
 
   if (!userId) {
     return redirect("/");
   }
 
+  const purchase = await db.purchase.findUnique({
+    where: {
+      userId_courseId: {
+        userId,
+        courseId: course.id,
+      },
+    },
+  });
+
   return (
-    <div>
-      <div>CourseSidebar</div>
+    <div className="h-full border-r flex flex-col overflow-y-auto shadow-sm">
+      <div className="p-8 flex flex-col border-b">
+        <h1 className="font-semibold">{course.title}</h1>
+        {purchase && (
+          <div className="mt-10">
+            {/* <CourseProgress
+              variant="success"
+              value={progressCount}
+            /> */}
+          </div>
+        )}
+      </div>
+      <div className="flex flex-col w-full">
+        {course.chapters.map((chapter) => (
+          <CourseSidebarItem
+            key={chapter.id}
+            id={chapter.id}
+            label={chapter.title}
+            isCompleted={!!chapter.userProgress?.[0]?.isCompleted}
+            courseId={course.id}
+            isLocked={!chapter.isFree && !purchase}
+          />
+        ))}
+      </div>
     </div>
   );
 };
